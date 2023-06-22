b4c62e0043a9e0ac0edaa2ce09e7db4af48bd863
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-06-22 08:38:03 +0100

    working with updated form

--- studymates/base/urls.py
+++ studymates/base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", home, name="home"),
     path("room/<str:pk>/", room, name="room"),
     path("create-room/", createRoom, name="create-room"),
+    path("update-room/<str:pk>/", updateRoom, name="update-room"),
+
 ]
--- studymates/base/views.py
+++ studymates/base/views.py
@@ -35,3 +35,16 @@ def createRoom(request):
 
     context = {"form": form}
     return render(request, "base/room_form.html", context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)  # returns a form that is prefilled
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
