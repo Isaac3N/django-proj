019a11d3eb45e4e5ed2ef163671aee0560ab573b
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-06-26 05:36:53 +0100

    delete messages in rooms

--- studymates/base/urls.py
+++ studymates/base/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path("create-room/", createRoom, name="create-room"),
     path("update-room/<str:pk>/", updateRoom, name="update-room"),
     path("delete-room/<str:pk>/", deleteRoom, name="delete-room"),
+    path("delete-message/<str:pk>/", deleteMessage, name="delete-message"),
+
 
 ]
